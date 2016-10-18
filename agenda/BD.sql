290f665cfea2186b3cef593c89b4a32c0e71efc7
Tom Farias <tom.robson@hotmail.com>
2016-10-18 16:05:18 -0200

    organizando BD

--- agenda/BD.sql
+++ agenda/BD.sql
@@ -3,10 +3,23 @@ create database bd_agenda;
 use bd_agenda;
 
 create table tb_pessoa(
-     id_pessoa          int                  not null,
-     tx_nome            varchar(100)  not null,
-     tx_sexo             char                not null,
-     ni_idade            int                   not null,
-     tx_fone              varchar(15)    not null,
+     id_pessoa          auto_increment        not null,
+     tx_nome            varchar(100)          not null,
+     tx_sexo            char                  not null,
+      dataNascimento          int                   not null,
+     tx_fone            varchar(15)           not null,
      constraint pk_tb_pessoa primary key (id_pessoa)
-)engine=innodb;
+)engine=innodb; --Esse "engine=innodb" significa o que.
+
+create database bdAgenda;
+
+use bd_Agenda;
+
+create table pessoa(
+  id_pessoa       auto_increment      not null,
+  nome            varchar(100)        not null,
+  sexo            char                not null,
+  nascimento      int                 not null,
+  telefone        varchar(17)         not null,
+  constraint pk_pessoa primary key (id_pessoa)
+);
