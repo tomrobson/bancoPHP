2ccc6baaa2f2b99546ab87cfcf1586511d2161b0
Tom Farias <tom.robson@hotmail.com>
2016-10-18 17:28:33 -0200

    d

--- agenda/index.php
+++ agenda/index.php
@@ -2,12 +2,12 @@
   $conexao = mysqli_connect("localhost", "root", "123");
 
   if($conexao){
-    if(mysqli_select_db($conexao, "db_agenda")){
-      echo "Conectado com sucesso.";
+    if(mysqli_select_db($conexao, "bd_agenda")){
+      //echo "Conectado com sucesso.";
     }else{
       echo "Não conecta ao DB.";
     }
-  }else {
+  }else{
     echo "Não conecta no localhost.";
   }
 ?>
@@ -16,6 +16,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="master.css" style="styles/css">
     <title>Agenda de Contatos</title>
   </head>
   <body>
@@ -31,14 +32,38 @@
         $sql = "select nome, sexo, nascimento, telefone from pessoa";
         $contato = mysqli_query($conexao, $sql);
 
-        if($mysqli_fetch_array($contato)==0) {
+        if(mysqli_fetch_array($contato)==0) {
       ?>
           <tr>
             <td id="vazio">Nenhum contato encontrado!</td>
           </tr>
       <?php
         }
-
+        else {
+          while ($linha = mysqli_fetch_array($contato)) {
+      ?>
+          <tr>
+            <td><?= $linha["nome"] ?></td>
+            <td>
+              <?php
+                if($linha["sexo"]=='M'){
+                  echo "Masculino";
+                }else{
+                  echo "Feminino";
+                }
+              ?>
+            </td>
+            <td>
+              <?php
+                $data = date-$linha["nascimento"];
+                echo $data;
+              ?>
+            </td>
+            <td><?= $linha["telefone"] ?></td>
+          </tr>
+      <?php
+          }
+        }
       ?>
     </table>
   </body>
